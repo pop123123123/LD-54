e296b586868cb5615cfd58c3d179392862f4c125
ValerianC <valeriancoulon@gmail.com>
2023-10-02 23:09:04 +0200

    Music loopee

--- src/MainScene/DJ.gd
+++ src/MainScene/DJ.gd
@@ -8,15 +8,19 @@ func _ready():
 	_start_reggae()
 	
 func _start_reggae():
-	whitePastaReggae.play();
-	whitePastaReggae.volume_db = 0;
-
+	whitePastaReggae.volume_db = 0
+	whitePastaReggae.play()
+	
 func _start_night_music():
+	watiByNight.volume_db=0
+	watiByNight.play()
+
+func _transition_night_music():
 	var tween = get_tree().create_tween()
 	tween.tween_property(whitePastaReggae, "volume_db", Vector2(whitePastaReggae.volume_db, -80), 2)
 	tween.tween_callback(func(): whitePastaReggae.stop(); watiByNight.volume_db=0; watiByNight.play())
 
-func _restart_reggae():
+func _transition_reggae():
 	var tween = get_tree().create_tween()
 	tween.tween_property(watiByNight, "volume_db", Vector2(watiByNight.volume_db, -80), 2)
-	tween.tween_callback(func(): watiByNight.stop(); whitePastaReggae.volume_db=0; whitePastaReggae.play())
+	tween.tween_callback(func(): watiByNight.stop(); _start_reggae())
